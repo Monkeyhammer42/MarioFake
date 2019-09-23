66067b35e24efd0ce66f1499a3237f1f20f2bce2
monkeyhammer42 <michael-whiddett@hotmail.com>
2019-09-23 14:53:37 +0100

    Contains all Assets Added player Move Shoot and jump animations and functions Added snail move and death animations and functions Added Beetle move and death animations and functions

---
+++ Assets/Scripts/Enemy Scripts/SnailScript.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnailScript : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    private Rigidbody2D myBody;
+    private Animator anim;
+
+    public LayerMask playerLayer;
+
+    private bool moveLeft;
+    private bool canMove;
+    private bool stunned;
+
+    public Transform left_Collision, right_Collision, top_Collision, down_Collision;
+    private Vector3 left_Collision_Position, right_Collision_Position;
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        left_Collision_Position = left_Collision.position;
+        right_Collision_Position = right_Collision.position;
+
+
+
+
+    }
+    void Start()
+    {
+        moveLeft = true;
+        canMove = true;
+
+    }
+
+
+    void Update()
+
+    { if (canMove)
+        {
+            if (moveLeft)
+            {
+                myBody.velocity = new Vector2(-moveSpeed, myBody.velocity.y);
+            }
+            else
+            {
+                myBody.velocity = new Vector2(moveSpeed, myBody.velocity.y);
+            }
+            
+        }
+        CheckCollision();
+    }
+
+    void CheckCollision() {
+        RaycastHit2D leftHit = Physics2D.Raycast(left_Collision.position, Vector2.left, 0.1f, playerLayer);
+        RaycastHit2D rightHit = Physics2D.Raycast(right_Collision.position, Vector2.left, 0.1f, playerLayer);
+
+        Collider2D topHit = Physics2D.OverlapCircle(top_Collision.position, 0.2f, playerLayer);
+        if (topHit != null)
+        {
+            if (topHit.gameObject.tag == MyTags.PLAYER_TAG)
+            {
+                if (!stunned)
+                {
+                    topHit.gameObject.GetComponent<Rigidbody2D>().velocity =
+                        new Vector2(topHit.gameObject.GetComponent<Rigidbody2D>().velocity.x, 7f);
+
+                    canMove = false;
+                    myBody.velocity = new Vector2(0, 0);
+
+                    anim.Play("SnailStunned");
+                    stunned = true;
+
+                    if (tag == MyTags.BEETLE_TAG) 
+                    {
+                        anim.Play("Stunned");
+                        StartCoroutine(Dead(0.5f));
+                    }
+                }
+            }
+        }
+
+        if (leftHit) {
+            if (leftHit.collider.gameObject.tag == MyTags.PLAYER_TAG) {
+                if (!stunned)
+                {
+
+                }
+                else {
+                    if (tag != MyTags.BEETLE_TAG)
+                    {
+                        myBody.velocity = new Vector2(15f, myBody.velocity.y);
+                        StartCoroutine(Dead(3f));
+                    }
+                }
+            }
+        }
+        if (rightHit)
+        {
+            if (rightHit.collider.gameObject.tag == MyTags.PLAYER_TAG)
+            {
+                if (!stunned)
+                {
+
+                }
+                else
+                {
+                    if (tag != MyTags.BEETLE_TAG)
+                    {
+                        myBody.velocity = new Vector2(-15f, myBody.velocity.y);
+                        StartCoroutine(Dead(3f));
+                    }
+                }
+            }
+        }
+
+
+        if (!Physics2D.Raycast (down_Collision.position, Vector2.down, 0.1f)) {
+
+            ChangeDirection();
+
+
+        }
+    }
+
+    void ChangeDirection() {
+
+        moveLeft = !moveLeft;
+
+        Vector3 tempScale = transform.localScale;
+
+        if (moveLeft)
+        {
+            tempScale.x = Mathf.Abs(tempScale.x);
+            left_Collision.position = left_Collision_Position;
+           right_Collision.position = right_Collision_Position;
+        }
+        else {
+            tempScale.x = -Mathf.Abs(tempScale.x);
+            left_Collision.position = right_Collision_Position;
+            right_Collision.position = left_Collision_Position;
+        }
+
+        transform.localScale = tempScale;
+
+    }
+
+    IEnumerator Dead(float timer) {
+        yield return new WaitForSeconds(timer);
+        gameObject.SetActive(false);
+            }
+    
+
+
+}//class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
